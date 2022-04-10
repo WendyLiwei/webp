e7785101e9ab67668895bb1decc7c9e475fdd921
WendyHsieh <wendy1218girl.com>
2022-04-10 23:25:34 +0800

    Commit Message...

---
+++ hw4/hw4.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $('input').click(function(){
+        setInterval(generate,700);
+        document.addEventListener('keydown',logKey);
+    });
+});
+
+function generate(){
+    var road_List=[];
+    for(var j=0;j<6;j++){
+        road_List[j]=Math.floor(Math.random()*26)+1;
+        var new_Word=document.createElement('img');
+        new_Word.src='alphabet/'+road_List[j]+'.png';
+        $(".road")[j].append(new_Word);
+
+        if($('.road')[j].children[0].style.left >'96%')
+            stopGame();
+    }
+    $(".road").find("img").animate({left:"97%"},10000);
+}
+
+var last_Word;
+function logKey(k){
+    var erase_Word=document.createElement('img');
+    erase_Word.src='alphabet/'+(k.keyCode-64)+'.png';
+
+    for(var j=0;j<6;j++){
+        last_Word=$('.road')[j].children[0];
+        if(last_Word.src==erase_Word.src){
+            $('.road')[j].removeChild(last_Word);
+            break;
+        }
+    }
+}
+
+function stopGame(){
+    alert("GAME OVER");
+}
