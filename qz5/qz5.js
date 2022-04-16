e553ffc3318926b746582c8ae061c01d8d1f7bc4
WendyLiwei <100269322+WendyLiwei@users.noreply.github.com>
2022-04-16 21:33:51 +0800

    Delete qz5 directory

--- qz5/qz5.js
+++
@@ -1,8 +0,0 @@
-$(document).click(function(e){
-  if ( e.target.tagName.toLowerCase() === 'li' ) {
-    console.log(e.target.textContent);
-  }
-});
-
-var textNode = "hello cgu!";
-$('#myList').append('<li>'+textNode+'</li>');
